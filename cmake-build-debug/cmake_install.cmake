e35aba6dce806fa84a4eb59f684c4562d438f3a8
Szotkowski <mikiszotkowski@protonmail.com>
2025-04-14 15:16:32 +0200

    rename

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Michael Szotkowski/CLionProjects/ASCII-Game
+# Install script for directory: C:/Users/Michael Szotkowski/CLionProjects/ElderGlyph
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Michael Szotkowski/CLionProjects/ASCII-Game/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Michael Szotkowski/CLionProjects/ElderGlyph/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
